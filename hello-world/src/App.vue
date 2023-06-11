a88b7ca46ab679954250b645d0301ef96baf0a30
daemusin12 <engrleonelfelezario812>
2023-06-11 18:45:14 +0800

    Binding to Attributes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,6 +2,8 @@
   <div>{{ greet }} {{ name }}</div>
   <div v-text="channel"></div>
   <div v-html="hack"></div>
+  <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
 </template>
 
 <script>
@@ -13,6 +15,8 @@ export default {
       name: "Engr. Leonel J. Felezario",
       channel: "Hotdog",
       hack: `<a href="#" onclick="alert('All your base are belong to us!')">CLICK ME!<a/>`,
+      headingId: 'heading',
+      isDisabled: true
     };
   },
 };
