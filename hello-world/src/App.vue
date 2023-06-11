aeb9ae31848df2782938e67401afbe912e23a0cd
daemusin12 <engrleonelfelezario812>
2023-06-11 23:43:13 +0800

    conditional rendering

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -23,6 +23,17 @@
   <h2 :style="headerStyleObject">Style Object</h2>
   <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
   <div :style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
+  <h2 v-if="num === 0">The number is zero</h2>
+  <h2 v-else-if="num < 0">The number is negative</h2>
+  <h2 v-else-if="num > 0">The number is positive</h2>
+  <h2 v-else>Not a number</h2>
+  <template v-if="display">
+    <h2>Engr. Leonel J. Felezario</h2>
+    <h2>Hotdog</h2>
+    <h2>Vue</h2>
+  </template>
+  <h2 v-show="showElement">Using v-show</h2>
+  <h2 v-if="showElement">Using v-if</h2>
 </template>
 
 <script>
@@ -61,6 +72,9 @@ export default {
         backgroundColor: 'red',
         border: '1px solid darkred',
       },
+      num: "Hi",
+      display: true,
+      showElement: false,
     };
   },
 };
