6af21962cc39c14f73ce228e266b18547a046c3a
daemusin12 <engrleonelfelezario812>
2023-06-15 00:54:36 +0800

    Immediate and Deep Watchers

--- hello-world/src/WatchersApp.vue
+++ hello-world/src/WatchersApp.vue
@@ -5,6 +5,12 @@
     <button @click="volume += 2">Increase</button>
     <button @click="volume -= 2">Decrease</button>
   </div>
+  <input type="text" v-model="movie" />
+  <input type="text" v-model="movieInfo.title" />
+  <input type="text" v-model="movieInfo.actor" />
+  <div>
+    <button @click="movieList.push('Wonder Woman')">Add Movie</button>
+  </div>
 </template>
 
 <script>
@@ -12,13 +18,18 @@ export default {
     name: 'App',
     data() {
         return {
-          volume: 0
+          volume: 0,
+          movie: 'Batman',
+          movieInfo: {
+            title: '',
+            actor: '',
+          },
+          movieList: ['Batman', 'Superman']
         };
     },
     methods: {
     },
-    computed: {
-    },
+    computed: {},
     watch: {
       volume(newValue, oldValue) {
         if (newValue === 16 && newValue > oldValue) {
@@ -26,6 +37,26 @@ export default {
             'Listening to a high volume for a long time may damage your hearing'
           )
         }
+      },
+      movie: {
+        handler(newValue) {
+          console.log(`Calling API with movie name = ${newValue}`)
+        },
+        immediate: true,
+      },
+      movieInfo: {
+        handler(newValue) {
+          console.log(
+            `Calling API with movie title = ${newValue.title} and actor = ${newValue.actor}`
+          )
+        },
+        deep: true
+      },
+      movieList: {
+        handler(newValue) {
+          console.log(`Updated list ${newValue}`)
+        },
+        deep: true
       }
     }
 }
