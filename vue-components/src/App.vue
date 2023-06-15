86f96a812e10f118b2df6d3361f8f6a0c5fa9deb
daemusin12 <engrleonelfelezario812>
2023-06-15 17:28:14 +0800

    provide and inject

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,25 +1,31 @@
 <template>
   <!-- <GreetHello name="Lucho" heroName="Spiderman"/>
   <GreetHello :name="name" :heroName="channel"/> -->
-  <ArticleComponent id="my-article"  :likes="50" :isPublished="true"/>
+  <!-- <ArticleComponent id="my-article"  :likes="50" :isPublished="true"/> -->
+  <h3>AppComponent username {{ name }}</h3>
+  <ComponentC />
 </template>
 
 <script>
 // import GreetHello from './components/GreetHello.vue'
-import ArticleComponent from './components/ArticleComponent.vue'
+// import ArticleComponent from './components/ArticleComponent.vue'
+import ComponentC from './components/ComponentC.vue'
 
 export default {
   name: 'App',
   components: {
-    ArticleComponent
+    ComponentC,
   },
   data() {
     return {
-      name: 'Leozzio',
-      channel: 'Hotdog'
+      name: 'Engr. Leonel J. Felezario'
     }
-  }
-
+  },
+  provide() {
+      return {
+        username: this.name
+      }
+    }
 }
 </script>
 
---
+++ vue-components/src/components/ComponentC.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Component C</h2>
+  <ComponentE />
+</template>
+
+<script>
+import ComponentE from './ComponentE'
+export default {
+    name: 'ComponentC',
+    components: {
+        ComponentE,
+    },
+}
+</script>
+
+<style>
+
+</style>
