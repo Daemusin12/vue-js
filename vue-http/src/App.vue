20431398cb74f63b09748b2c2f695a75f481af32
daemusin12 <engrleonelfelezario812>
2023-06-16 02:03:24 +0800

    HTTP POST Request

--- vue-http/src/App.vue
+++ vue-http/src/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <CreatePost />
   <PostList />
 </template>
 
 <script>
 import PostList from './components/PostList.vue'
+import CreatePost from './components/CreatePost.vue'
 export default {
   name: 'App',
   components: {
     PostList,
+    CreatePost
   }
 }
 </script>
