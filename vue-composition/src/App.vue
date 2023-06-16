fc4c59ed3cd1003d62facf3943714a6287fbf5c7
daemusin12 <engrleonelfelezario812>
2023-06-16 18:14:30 +0800

    Replacing data with ref

--- vue-composition/src/App.vue
+++ vue-composition/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    
+    <Data />
   </div>
 </template>
 
 <script>
+import Data from './components/Data.vue';
 
 export default {
   name: 'App',
+  components: {
+    Data
+  }
 }
 </script>
 
---
+++ vue-composition/src/components/Data.vue
@@ -0,0 +1,32 @@
+<template>
+    <h2>Options - {{ o_firstName }}</h2>
+    <h2>Composition - {{ c_firstName }}</h2>
+    <h2>Composition - {{ greet }}</h2>  
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    name: 'DataComponent',
+    setup() {
+        const c_firstName = ref('Clark')
+        c_firstName.value = 'Diana'
+        const greet = `Hello ${c_firstName.value}`
+        console.log('c_firstName', c_firstName)
+
+        return {
+            c_firstName,
+            greet
+        }
+    },
+    data () {
+        return {
+            o_firstName: 'Leozzio'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
