2ab80ba32162c6a9486001805d03dd0380faa8a0
daemusin12 <engrleonelfelezario812>
2023-06-19 18:30:44 +0800

    Replacing Watchers (Part 2)

--- vue-composition/src/components/Watch.vue
+++ vue-composition/src/components/Watch.vue
@@ -4,14 +4,42 @@
 
     <input type="text" placeholder="First Name" v-model="firstName" />
     <input type="text" placeholder="Last Name" v-model="lastName" />
+
+    <input type="text" placeholder="Reactive First Name" v-model="fName" />
+    <input type="text" placeholder="Reactive Last Name" v-model="lName" />
+    <input type="text" placeholder="Reactive Hero Name" v-model="options.heroName" />
   </div>
 </template>
 
 <script>
-import { ref, watch } from 'vue'
+import { ref, watch, reactive, toRefs } from 'vue'
+import _ from 'lodash'
 export default {
     name: 'WatchComponent',
     setup() {
+
+        const state = reactive({
+        fName: '',
+        lName: '',
+        options: {
+            heroName: ''
+        }
+        })
+
+        // watch(() => {
+        //     return { ...state }
+        // }, function(newValue, oldValue) {
+        //     console.log('fName Old value', oldValue.fName)
+        //     console.log('fName New value', newValue.fName)
+        //     console.log('lName Old value', oldValue.lName)
+        //     console.log('lName New value', newValue.lName)
+        // })
+
+        watch(() => _.cloneDeep(state.options), function(newValue, oldValue) {
+            console.log('fname Old value', oldValue)
+            console.log('fname New value', newValue)
+        }, {deep: true,})
+
         const firstName = ref('')
         const lastName = ref('Wayne')
 
@@ -26,7 +54,8 @@ export default {
 
         return {
             firstName,
-            lastName
+            lastName,
+            ...toRefs(state)
         }
     },
     data() {
