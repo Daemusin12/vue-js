2460842c043ceff0473553a4e352d62cf9c5134d
daemusin12 <engrleonelfelezario812>
2023-06-20 17:11:23 +0800

    Template Refs and Composition API

--- vue-composition/src/App.vue
+++ vue-composition/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <ProvideInject />
+    <TemplateRef />
   </div>
 </template>
 
 <script>
-import ProvideInject from './components/ProvideInject.vue';
+import TemplateRef from './components/TemplateRef.vue';
 
 export default {
   name: 'App',
   components: {
-    ProvideInject
+    TemplateRef
   }
 }
 </script>
